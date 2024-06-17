ba8fc889cdaf199a1e9b1127ae2bad61a21f59bf
benzidane027 <benzidan27@yahoo.com>
2024-06-17 17:38:58 +0100

    some dev

--- backup_script.sh
+++ backup_script.sh
@@ -1,54 +1,73 @@
 #!/bin/bash
 
 # Configuration
-CONTAINER_NAME="container_odoo"
-DB_NAME="Real"
+CONTAINER_NAME="container_database"
+DB_NAME="REAL"
 DB_USER="odoo"
 DB_PASSWORD="odoo"
-BACKUP_DIR="/home/solvex/service.backup"
 
-BACKUP_FILE="$BACKUP_DIR/$DB_NAME-$(date +%Y%m%d%H%M%S).sql"
-PROXMOX_REPO="10.0.0.12"
-PROXMOX_USER="root@pam"
-PROXMOX_PASSWORD="valkiry"
+BACKUP_NEW_DIR="/home/solvex/service.backup/backup_$(date +%d_%m_%Y)"
+BACKUP_OLD_DIR="/home/solvex/service.backup/backup_old"
+FILESRORE_PATH="/home/solvex/service.odoo/filestore/filestore"
+EXTRA_ADDONS_PATH="/home/solvex/service.odoo/extra-addons"
+
+DATE="$(date +%S_%M_%H_%d_%m_%Y)"
+DATABASE_BACKUP_FILE="$BACKUP_NEW_DIR/database_${DB_NAME}_${DATE}.sql.gz"
+FILESTORE_BACKUP_FILE="$BACKUP_NEW_DIR/filestore_${DB_NAME}_${DATE}.zip"
+EXTRA_ADDONS_BACKUP_FILE="$BACKUP_NEW_DIR/extra_addons_${DB_NAME}_${DATE}.zip"
+
+PROXMOX_REPO="root@pam@10.0.0.12:8007:backup-container"
 
 # Create backup directory if it doesn't exist
-mkdir -p "$BACKUP_DIR"
+mkdir -p "$BACKUP_NEW_DIR"
+mkdir -p "$BACKUP_OLD_DIR"
 
 # Dump the PostgreSQL database from the Docker container
 echo "Dumping PostgreSQL database from Docker container..."
-docker exec -e PGPASSWORD="$DB_PASSWORD" "$CONTAINER_NAME" pg_dump -U "$DB_USER" -d "$DB_NAME" > "$BACKUP_FILE"
+docker  exec -t "$CONTAINER_NAME" pg_dump  -U "$DB_USER" -d "$DB_NAME" | bzip2 --best > "$DATABASE_BACKUP_FILE"
+if [ $? -ne 0 ]; then
+  echo "database failed! to dumped"
+else
+  echo "database dumped successful."
+fi
+
+echo "valkiry0024"| sudo -S zip -r  "$FILESTORE_BACKUP_FILE" "$FILESRORE_PATH/$DB_NAME"
 
 if [ $? -ne 0 ]; then
-  echo "Database dump failed!"
-  exit 1
+  echo "filestore failed! to dumped"
 else
-  echo "Database dump completed: $BACKUP_FILE"
+  echo "filestore dumped successful."
 fi
+
+echo "valkiry0024"| sudo -S zip -r  "$EXTRA_ADDONS_BACKUP_FILE" "$EXTRA_ADDONS_PATH" 
+
+if [ $? -ne 0 ]; then
+  echo "extra-addons failed! to dumped"
+
+else
+  echo "extra-addons dumped successful."
+fi
+
 
 # Login to Proxmox Backup Server
 echo "Logging in to Proxmox Backup Server..."
-echo "$PROXMOX_PASSWORD" | proxmox-backup-client login --repository "$PROXMOX_REPO" --username "$PROXMOX_USER"
+proxmox-backup-client login --repository "$PROXMOX_REPO"
 
 if [ $? -ne 0 ]; then
   echo "Proxmox login failed!"
-  exit 1
 else
   echo "Proxmox login successful."
 fi
 
-# Upload the backup file
+#Upload the backup file
 echo "Uploading backup file to Proxmox Backup Server..."
-proxmox-backup-client upload "$BACKUP_FILE" --repository "$PROXMOX_REPO"
+proxmox-backup-client backup root.pxar:"$BACKUP_NEW_DIR" --repository "$PROXMOX_REPO"
 
 if [ $? -ne 0 ]; then
-  echo "File upload failed!"
-  exit 1
+  echo "Files upload failed!"
 else
-  echo "File upload successful."
+  echo "Files upload successful."
 fi
-
-echo "Backup process completed successfully."
 
 # Logout from Proxmox Backup Server
 echo "Logging out from Proxmox Backup Server..."
@@ -56,9 +75,16 @@ proxmox-backup-client logout --repository "$PROXMOX_REPO"
 
 if [ $? -ne 0 ]; then
   echo "Proxmox logout failed!"
-  exit 1
 else
   echo "Proxmox logout successful."
 fi
 
-exit 0
+mv "$BACKUP_NEW_DIR"/* "$BACKUP_OLD_DIR"
+echo "backup moved to $BACKUP_OLD_DIR successful."
+rm -rf "$BACKUP_NEW_DIR"
+echo "$BACKUP_NEW_DIR remove successful."
+
+cd "$BACKUP_OLD_DIR" || { echo "Directory not found: $BACKUP_OLD_DIR"; exit 1; }
+ls -tp | grep -v '/$' | tail -n +7 | xargs -I {} rm -- {}
+
+
